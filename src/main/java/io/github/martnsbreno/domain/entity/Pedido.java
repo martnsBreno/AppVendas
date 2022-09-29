34353892a9904b4eb0f43859461837e02c1885c3
Breno Martins <102875429+martnsBreno@users.noreply.github.com>
2022-09-29 13:03:50 -0300

    migrando a db para o mysql

--- src/main/java/io/github/martnsbreno/domain/entity/Pedido.java
+++ src/main/java/io/github/martnsbreno/domain/entity/Pedido.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Table(name = "pedido")
 public class Pedido {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
